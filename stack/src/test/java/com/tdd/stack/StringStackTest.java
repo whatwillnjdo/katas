0125dd7493f83dd82c296d5f14749ab77e5dde5f
Steve Katra <steve.katra@wolterskluwer.com>
2017-09-30 17:29:33 -0400

    fixed test

--- stack/src/test/java/com/tdd/stack/StringStackTest.java
+++ stack/src/test/java/com/tdd/stack/StringStackTest.java
@@ -3,6 +3,7 @@ package com.tdd.stack;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class StringStackTest {
@@ -11,7 +12,7 @@ public class StringStackTest {
 
     @Before
     public void setUp() throws Exception {
-        stack = new Stack<>();
+        stack = new Stack();
     }
 
     @Test(expected = UnderflowException.class)
